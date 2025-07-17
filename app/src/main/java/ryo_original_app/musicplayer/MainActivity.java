14c2d1d3820c399a750eaf4a7244ea99f126fcb3
ryo <r.yoshimoto.work@gmail.com>
2025-07-17 14:29:39 +0900

    ・音楽データの取得 ・取得したデータから情報をUI側へ表示（タイトル、楽曲時間、アートファイル） ・メソッド名などの変更 ・楽曲データを表示できるようUI調整

--- app/src/main/java/ryo_original_app/musicplayer/MainActivity.java
+++ app/src/main/java/ryo_original_app/musicplayer/MainActivity.java
@@ -3,6 +3,8 @@ package ryo_original_app.musicplayer;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -31,13 +34,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton _btBack;            // 戻るボタン
     private ImageButton _btNext;            // 次へボタン
     private ImageView _artFile;             // ジャケットファイル
+    private TextView _tuneTitle;            // 楽曲タイトル
+    private TextView _tuneTotalTime;        // 1楽曲の総時間
+    private TextView _tuneNowTime;          // 1楽曲の今の再生時間
 
     /* 音楽ファイル関係 */
     private File fileDir;                   // 楽曲ディレクトリ
-    private File songList[];                // 楽曲一覧
-    private int totalSongNum = 0;           // 総楽曲数
-    private int nowSongNum = 0;             // 楽曲番号
+    private File tunesList[];                // 楽曲一覧
+    private int totalTunesNum = 0;           // 総楽曲数
+    private int nowTuneNum = 0;             // 楽曲番号
 
+    /* 正規表現用String */
+    private String mp3String = ".mp3";
+    private String wavString = ".wav";
+    private String m4aString = ".m4a";
 
     /*
      * 再生か一時停止化のフラグ
@@ -51,8 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // カスタムクラッシュハンドラを設定
-        Thread.setDefaultUncaughtExceptionHandler(new CustomExceptionHandler());
+
+        Thread.setDefaultUncaughtExceptionHandler(new CustomExceptionHandler());    // カスタムクラッシュハンドラを設定
 
         /* スプラッシュのための中断処理 */
         try {
@@ -61,8 +71,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             super.onDestroy();
         }
 
-        // 権限を得る処理
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_AUDIO}, 1);
+        /* 権限を得る処理 */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_AUDIO}, 1);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
 
         /* 以下メイン画面描画用処理 */
         setTheme(R.style.Base_Theme_MusicPlayer);
@@ -87,14 +101,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    // requestPermissionsのコールバック
+    /* requestPermissionsのコールバック */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults.length > 0 &&
                 grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "権限が許可されました", Toast.LENGTH_SHORT).show();
-            songList = createMusicList();
+            tunesList = createTunesList();
         }  else {
             Toast.makeText(this, "音楽とオーディオの権限を許可してください", Toast.LENGTH_SHORT).show();
             String uriString = "package:" + getPackageName();
@@ -105,14 +119,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /* 再生・停止ボタン押下処理 */
     public void onPlayButton(View v) {
-        if(v.getId() == R.id.btPlay){
-            if(btPlayFlag == 0) {
-                _btPlay.setImageResource(R.drawable.stop);
-                btPlayFlag = 1;
-            } else if(btPlayFlag == 1) {
-                _btPlay.setImageResource(R.drawable.start);
-                btPlayFlag = 0;
-            }
+        if(btPlayFlag == 0) {
+            _btPlay.setImageResource(R.drawable.stop);
+            btPlayFlag = 1;
+        } else if(btPlayFlag == 1) {
+            _btPlay.setImageResource(R.drawable.start);
+            btPlayFlag = 0;
         }
     }
 
@@ -130,56 +142,105 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        if(v.getId() == R.id.btPlay){
+            onPlayButton(v);
+        } else if(v.getId() == R.id.btBack) {
+            onBackButton(v);
+        } else if(v.getId() == R.id.btNext) {
+            onNextButton(v);
+        }
     }
 
     /* 楽曲データ取得 */
-    private File[] createMusicList() {
-        System.out.println("音楽リスト作成");
+    private File[] createTunesList() {
+        String path = Environment.getExternalStorageDirectory().getPath();  // パス生成
+        fileDir = new File(path + "/Music/");                      // Fileクラスのオブジェクトを生成する
+        tunesList = fileDir.listFiles();
+        File[] repairTunesList = null;                                       // 整形後の楽曲リストの定義
+        totalTunesNum = 0;                                                   // 総楽曲数の初期化
 
-        // パス生成
-        String path = Environment.getExternalStorageDirectory().getPath();
-        // Fileクラスのオブジェクトを生成する
-        fileDir = new File(path + "/Music/");
-        File[] songList = fileDir.listFiles();
-        // 整形後の楽曲リストの定義
-        File[] repairSongList = null;
-
-        // 楽曲がある場合、楽曲データの整形を行う
-        if (songList != null) {
-            int repairSongLength = 0;
-            // ディレクトリからファイルのみ抽出して、楽曲数を数える
-            for (File file : songList) {
-                if (file.isFile()) {
-                    repairSongLength++;
+        /* 楽曲がある場合、楽曲データの整形を行う */
+        if (tunesList != null) {
+            int repairTuneLength = 0;                                       // 整形後の楽曲数
+            /* ディレクトリからファイルのみ抽出して、楽曲数を数える
+            *  対応拡張子だけ抜粋
+            * */
+            for (File file : tunesList) {
+                String filenameString = file.toString();
+                if (file.isFile()
+                        && (filenameString.toLowerCase().endsWith(mp3String)
+                        || filenameString.toLowerCase().endsWith(wavString)
+                        || filenameString.toLowerCase().endsWith(m4aString))) {
+                    repairTuneLength++;
                 }
             }
-            // 正確な楽曲数を基にしたFile配列
-            repairSongList = new File[repairSongLength];
+            repairTunesList = new File[repairTuneLength];                    // 正確な楽曲数を基にしたFile配列
 
-            for (File file : songList) {
-                // 非整形楽曲データ配列を整形用配列に放り込む
-                if (file.isFile()) {
-                    repairSongList[totalSongNum] = file;
-                    totalSongNum++;
+            for (File file : tunesList) {
+                String filenameString = file.toString();
+                /* 非整形楽曲データ配列を整形用配列に放り込む
+                *  対応拡張子だけ抜粋
+                * */
+                if (file.isFile()
+                        && (filenameString.toLowerCase().endsWith(mp3String)
+                        || filenameString.toLowerCase().endsWith(wavString)
+                        || filenameString.toLowerCase().endsWith(m4aString))) {
+                    repairTunesList[totalTunesNum] = file;
+                    totalTunesNum++;
                 }
             }
+
+            /* 最初の楽曲データ取り出し */
+            if (totalTunesNum > 0) {
+                nowTune(0);
+            }
+
         }
 
-        return repairSongList;
+        return repairTunesList;
+    }
+
+    private void nowTune(int i) {
+        /* 楽曲UI各種定義 */
+        _tuneTitle = findViewById(R.id.tuneTitle);
+        _tuneTotalTime = findViewById(R.id.tuneTotalTime);
+        _tuneNowTime = findViewById(R.id.tuneNowTime);
+
+        /* メタデータ取り出し */
+        Uri mediaFileUri = Uri.parse(tunesList[i].toString());  // 音声ファイルのURI文字列を元にURIオブジェクトを生成
+        MediaMetadataRetriever tuneData = new MediaMetadataRetriever(); // メタ情報取り出しのためのクラス
+        tuneData.setDataSource(mediaFileUri.toString());        // URIをもとにデータをセットする
+        String tuneTitle = tuneData.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);         // 楽曲タイトル
+        String tuneTotalTime = tuneData.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);  // 楽曲時間（ミリ秒）
+
+        /* 楽曲時間（ミリ秒）の変換作業 */
+        int secTmp = Integer.parseInt(tuneTotalTime) / 1000;    // ミリ秒を秒に
+        int tuneTotalTimeMin = (secTmp % 3600) / 60;            // 秒を分化し分のみを抽出
+        int tuneTotalTimeSec = secTmp % 60;                     // 秒を60で割った余りが分を除いた秒になる
+        tuneTotalTime = (tuneTotalTimeMin + ":" + tuneTotalTimeSec);
+
+        _tuneTitle.setText(tuneTitle);
+        _tuneTotalTime.setText(tuneTotalTime);
+
+        /* アートファイルの導入 */
+        byte[] data = tuneData.getEmbeddedPicture();    // メタファイルから取ったアートファイルをバイト配列に入れる
+        if (null != data) { // データが無ければnullにする
+            _artFile.setImageBitmap(BitmapFactory.decodeByteArray(data, 0, data.length));   // 画像データの代入
+        }
     }
 
     /*
      * TODO:全体実装後とりかかる
      * 楽曲データリピート
      **/
-    private void repeatMusicData() {
+    private void repeatTuneData() {
     }
 
     /*
      * TODO:全体実装後とりかかる
      * 楽曲データシャッフル
      **/
-    private void shuffleMusicData() {
+    private void shuffleTuneData() {
     }
 
     /*
@@ -188,17 +249,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      **/
     public void onSubMenu(View v) {
 
-        // 楽曲タイトルを配列化
-        String[] songListTitle = new String[totalSongNum];
+        String[] tunesListTitle = new String[totalTunesNum];  // 楽曲タイトルを配列化
 
-        // 楽曲タイトルをFile配列からString配列に代入
-        for(int i = 0; i < songList.length; i++) {
-            songListTitle[i] = songList[i].getName();
+        /* 楽曲タイトルをFile配列からString配列に代入 */
+        for(int i = 0; i < tunesList.length; i++) {
+            tunesListTitle[i] = tunesList[i].getName();
         }
 
-        // 次画面への準備
-        Intent intent = new Intent(this, SongList.class);
-        intent.putExtra("songList", songListTitle);
+        /* 次画面への準備 */
+        Intent intent = new Intent(this, TunesList.class);
+        intent.putExtra("tunesList", tunesListTitle);
         startActivity(intent);
     }
 }
--- app/src/main/java/ryo_original_app/musicplayer/TunesList.java
+++ app/src/main/java/ryo_original_app/musicplayer/TunesList.java
@@ -12,26 +12,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Arrays;
 import java.util.List;
 
-public class SongList extends AppCompatActivity implements View.OnClickListener{
+public class TunesList extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /* 楽曲リスト画面遷移 */
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.song_list);
+        setContentView(R.layout.tunes_list);
 
         // 前画面から楽曲リストを取得
         Intent intent = getIntent();
-        String[] songList = intent.getStringArrayExtra("songList");
+        String[] tuneList = intent.getStringArrayExtra("tunesList");
 
         // 画面定義
-        ListView setSongList = findViewById(R.id.songsListMenu);
-        TextView setTotalSongNum = findViewById(R.id.totalSongNum);
+        ListView setTunesList = findViewById(R.id.tunesListMenu);
+        TextView setTotalTunesNum = findViewById(R.id.tunesTotalNum);
 
-        List<String> list = Arrays.asList(songList);
+        List<String> list = Arrays.asList(tuneList);
 
-        ArrayAdapter adapter = new ArrayAdapter(SongList.this,android.R.layout.simple_list_item_1,list);
-        setSongList.setAdapter(adapter);
-        setTotalSongNum.setText("楽曲リスト:全" + songList.length + "曲");
+        ArrayAdapter adapter = new ArrayAdapter(TunesList.this,android.R.layout.simple_list_item_1,list);
+        setTunesList.setAdapter(adapter);
+        setTotalTunesNum.setText("楽曲リスト:全" + tuneList.length + "曲");
     }
 
     /* バツボタン押下時の処理　メイン画面に戻る */
